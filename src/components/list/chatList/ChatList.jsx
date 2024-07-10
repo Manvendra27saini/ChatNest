35f2d3a9e68b7ab14e396be1eeaca1fafcd94588
Manvendra saini <121102169+Manvendra27saini@users.noreply.github.com>
2024-07-10 11:12:58 +0530

    Added components

---
+++ src/components/list/chatList/ChatList.jsx
@@ -0,0 +1,8 @@
+import "./chatList.css"
+
+const ChatList = () => {
+    return (
+        <div className= 'list'>ChatList</div>
+    )
+}
+export default ChatList
---
+++ src/components/list/userInfo/Userinfo.jsx
@@ -0,0 +1,11 @@
+import "/.userInfo.css"
+
+const Userinfo = () => {
+    return (
+        <div className= 'userInfo'>
+            <div className="user"></div>
+            <div className="icons"></div>
+        </div>
+    )
+}
+export default Userinfo
