55601f7aacbe32af1703930e744e237de5987ade
Manvendra27saini <manvendrasaini2005@gmail.com>
2024-07-13 02:27:58 +0530

    Chat Detail Component of other user

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -1,7 +1,64 @@
 import "./detail.css"
 const Detail = () => {
     return (
-        <div className= 'detail'>Detail</div>
+        <div className= 'detail'>
+            <div className="user">
+                <img src="./avatar.png" alt="" />
+                <h2>Ohm</h2>
+                <p>Lorem ipsum dolor, sit amet </p>
+            </div>
+            <div className="info">
+                <div className="option">
+                    <div className="title">
+                        <span>Chat Settings</span>
+                        <img src="./arrowUp.png" alt="" />
+                    </div>
+                </div>
+                <div className="option">
+                    <div className="title">
+                        <span>Chat Settings</span>
+                        <img src="./arrowDown.png" alt="" />
+                    </div>
+                </div>
+                <div className="option">
+                    <div className="title">
+                        <span>Privacy & help</span>
+                        <img src="./arrowUp.png" alt="" />
+                    </div>
+                </div>
+                <div className="option">
+                    <div className="title">
+                        <span>Shared photos</span>
+                        <img src="./arrowDown.png" alt="" />
+                    </div>
+                    <div className="photos">
+                        <div className="photoItem">
+                            <div className="photoDetail">
+                            <img src="https://img.freepik.com/free-photo/abstract-autumn-beauty-multi-colored-leaf-vein-pattern-generated-by-ai_188544-9871.jpg?size=626&ext=jpg"  alt=""/>
+                            </div>
+                            <span>phot_2024_7.png</span>
+                            <img src="./download.png" alt="" className="icon"/>
+                        </div>
+                        <div className="photoItem">
+                            <div className="photoDetail">
+                            <img src="https://img.freepik.com/free-photo/abstract-autumn-beauty-multi-colored-leaf-vein-pattern-generated-by-ai_188544-9871.jpg?size=626&ext=jpg"  alt=""/>
+                            </div>
+                            <span>phot_2024_7.png</span>
+                            <img src="./download.png" alt="" className="icon"/>
+                        </div>
+                        
+                        
+                    </div>
+                </div>
+                <div className="option">
+                    <div className="title">
+                        <span>Shared Files</span>
+                        <img src="./arrowUp.png" alt="" />
+                    </div>
+                </div>
+                <button>Block User</button>
+            </div>
+        </div>
     )
 }
 export default Detail
